b911c95eb561a3df2b319009ba053f09c5082361
Dhananjoy Kumar Roy <dlswf.roy@gmail.com>
2026-02-28 01:07:11 +0000

    টপ গ্রেড টিউটোরিয়ালস এই নামটি কিভাবে আসতেছে

--- src/lib/school-info.ts
+++ src/lib/school-info.ts
@@ -18,11 +18,11 @@ export interface SchoolInfo {
 export const APP_ICON_URL = 'https://i.postimg.cc/zvsNP6qY/IMG-20260218-WA0002.jpg';
 
 export const defaultSchoolInfo: SchoolInfo = {
-    name: 'টপ গ্রেড টিউটোরিয়ালস',
-    nameEn: 'Top Grade Tutorials',
-    eiin: '000000',
+    name: 'বীরগঞ্জ পৌর উচ্চ বিদ্যালয়',
+    nameEn: 'Birganj Pouro High School',
+    eiin: '138244',
     code: '0000',
-    address: 'আপনার প্রতিষ্ঠানের ঠিকানা এখানে লিখুন',
+    address: 'বীরগঞ্জ, দিনাজপুর',
     logoUrl: APP_ICON_URL
 };
 
