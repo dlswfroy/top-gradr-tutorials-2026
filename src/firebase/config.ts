a419e64ad5ac49f1fff8c9001dd6c7adcb520264
Dhananjoy Kumar Roy <dlswf.roy@gmail.com>
2026-02-28 01:14:05 +0000

    // Import the functions you need from the SDKs you need import { initial

--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -9,11 +9,12 @@ import { FirebaseOptions } from 'firebase/app';
  * The Firebase App configuration object.
  */
 export const firebaseConfig: FirebaseOptions = {
-  apiKey: "AIzaSyAgqenE6ITASrZ9Jg7AkhRXUdpBCJX2JXg",
-  authDomain: "school-management-system-935.firebaseapp.com",
-  databaseURL: "https://school-management-system-935-default-rtdb.firebaseio.com",
-  projectId: "school-management-system-935",
-  storageBucket: "school-management-system-935.firebasestorage.app",
-  messagingSenderId: "883555716810",
-  appId: "1:883555716810:web:7ca86640f2fd30cb4b6e33"
+  apiKey: "AIzaSyAUZRXXpUzOuVhufOLafZ6NxnIq71pL27Q",
+  authDomain: "birganj-pouro-high-schoo-ce8e5.firebaseapp.com",
+  databaseURL: "https://birganj-pouro-high-schoo-ce8e5-default-rtdb.asia-southeast1.firebasedatabase.app",
+  projectId: "birganj-pouro-high-schoo-ce8e5",
+  storageBucket: "birganj-pouro-high-schoo-ce8e5.firebasestorage.app",
+  messagingSenderId: "316839573028",
+  appId: "1:316839573028:web:9c9dc33e497783cfde8ab1",
+  measurementId: "G-FEYQHXRX92"
 };
